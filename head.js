cd7ab3b6109d8bc8f8487cb6fd21226d013e21a4
masi846 <masii.shaneshin@gmail.com>
2020-11-28 19:44:04 -0500

    Refactor head

---
+++ head.js
@@ -0,0 +1,10 @@
+const assertEqual = require('./assertEqual');
+
+  
+var  head =  function(array) {
+ 
+
+}
+assertEqual(head([5,6,7]),  5 );
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
