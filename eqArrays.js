3724423af0d31d2395f505772fdd4a0d717eea34
masi846 <masii.shaneshin@gmail.com>
2020-11-28 20:03:40 -0500

    Refactor eqArray

---
+++ eqArrays.js
@@ -0,0 +1,13 @@
+const assertEqual = require('./assertEqual');
+
+const eqArrays = function(f_arr, s_arr) {
+
+//f_arr=[];
+//s_arr=[];
+
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArray;
+  
