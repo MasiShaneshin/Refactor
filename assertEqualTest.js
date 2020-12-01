493c7f956e6bae3b4b1b96dfc0abc89b25975c22
masi846 <masii.shaneshin@gmail.com>
2020-12-01 12:34:11 -0500

    unit test

---
+++ assertEqualTest.js
@@ -0,0 +1,16 @@
+const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+
+
+describe("#assertEqual", () => {
+
+  it("return Lighthouse Labs for[Lighthouse Labs]", () => {
+    assert.strictEqual(assertEqual(["Lighthouse Labs"]), "Lighthouse Labs");
+
+  });
+
+  
+
+
+
+});
