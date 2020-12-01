5186daf9666cf1b593fb2d3c46160029c9f499a7
Masi <72994325+MasiShaneshin@users.noreply.github.com>
2020-12-01 08:41:52 -0500

    Update head.js

--- head.js
+++ head.js
@@ -1,10 +1,16 @@
-const assertEqual = require('./assertEqual');
+const assert = require('chai').assert;
+const head = require('../head');
 
-  
-var  head =  function(array) {
- 
+describe("#head", () => {
 
-}
-assertEqual(head([5,6,7]),  5 );
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-module.exports = head;
+  it("returns 1 for [1, 2, 3]", () => {
+
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+
+it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+});
